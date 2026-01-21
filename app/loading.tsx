5f1c584a8b2684a94defffb1bfc4633c90dac929
danodoms <danodoms@gmail.com>
2026-01-21 11:38:06 +0800

    feat: remove user submission handling from loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,20 +1,6 @@
-import { upsertUser } from "@/lib/actions/users";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
 
-    async function handleSubmit(formData: FormData) {
-        "use server"
-        const username = formData.get("username") as string;
-
-        if (!username) return;
-
-        const result = await upsertUser(username);
-
-        if (result.success && result.data) {
-            redirect(`/todos/${result.data.id}`);
-        }
-    }
 
     return (
         <div className="flex flex-col min-h-screen w-full justify-center items-center gap-4">
